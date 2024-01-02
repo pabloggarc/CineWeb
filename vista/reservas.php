d3605d68bf88c0e2d57765d62bbb75ceec36481d
PedroArroyo16 <pedro.arroyo@edu.uah.es>
2024-01-02 13:44:22 +0100

    radio button

--- vista/reservas.php
+++ vista/reservas.php
@@ -47,13 +47,42 @@ Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Html.html to edit thi
             for ($j = 0; $j < count($butaca_fila[$i]); $j++) {
                 echo "<td><br>".$butaca_fila[$i][$j]. " - ".$butaca_columna[$i][$j]."</td>";
             }
-            echo "<td><button>SSFSDF</button></td>";
-            
+            echo '<td>
+            <input type="checkbox" name="color[]"></td>';
             echo "</tr>";
         }
+        echo '<button type="button" onclick="obtenerFilasSeleccionadas()">Borrar Selección</button>';
         ?>
     </table>
     </body>
+
+    <script>
+
+function obtenerFilasSeleccionadas() {
+            // Obtener todas las checkboxes
+            var checkboxes = document.getElementsByName('color[]');
+            
+            // Array para almacenar las filas seleccionadas
+            var filasSeleccionadas = [];
+
+            // Iterar sobre las checkboxes
+            for (var i = 0; i < checkboxes.length; i++) {
+                // Verificar si la checkbox está marcada
+                if (checkboxes[i].checked) {
+                    // Obtener la fila padre de la checkbox y agregarla al array
+                    var fila = checkboxes[i].closest('tr');
+                    filasSeleccionadas.push(fila.textContent.trim());
+                }
+            }
+
+            // Mostrar información de las filas seleccionadas (puedes hacer lo que quieras con esta información)
+            if (filasSeleccionadas.length > 0) {
+                alert('Filas seleccionadas:\n' + filasSeleccionadas.join('\n'));
+            } else {
+                alert('Ninguna fila seleccionada.');
+            }
+        }
+    </script>
 
 
 </html>
